4e78be0e4c0ea4c36dfd329122f010e565b89e17
Devin Sharpe <devin.sharpe@telrite.com>
2024-03-25 01:02:46 -0400

    feat: update web app font and tailwind config

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -2,7 +2,14 @@ import type { Config } from "tailwindcss";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
+    },
+  },
   plugins: [],
 };
 export default config;

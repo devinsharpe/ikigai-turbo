5c732825ba89767dc3c18db2e6ba64550fbb1296
Devin Sharpe <devin.sharpe@telrite.com>
2024-04-12 22:38:42 -0400

    feat: add Spinner component and update button

--- packages/ui/src/components/Spinner.tsx
+++ packages/ui/src/components/Spinner.tsx
@@ -1,3 +1,15 @@
 "use client";
 
-export default function Spinner 
+import { LoaderCircleIcon } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
+interface SpinnerProps extends React.SVGAttributes<SVGSVGElement> {}
+
+export default function Spinner({ className, ...props }: SpinnerProps) {
+  return (
+    <LoaderCircleIcon
+      {...props}
+      className={twMerge("animate-spin", className)}
+    />
+  );
+}

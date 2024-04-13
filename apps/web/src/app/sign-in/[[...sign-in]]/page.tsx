c8b5afd1596fb5ab5bab0f0abdc8f85c035558fa
Devin Sharpe <devin.sharpe@telrite.com>
2024-04-12 23:57:11 -0400

    feat: start sign in page

--- apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,57 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import React from "react";
+import { useSignIn } from "@clerk/nextjs";
+import AuthHeader from "~/components/AuthHeader";
+import AuthLayout from "~/layouts/Auth";
+import { Button, ButtonTheme } from "@repo/ui/components";
+import useAuthGuard from "~/hooks/useAuthGuard";
 
 export default function SignInPage() {
+  const { isLoaded, signIn, setActive } = useSignIn();
+
+  const [isLoadingApple, setIsLoadingApple] = React.useState(false);
+
+  useAuthGuard(true, "/");
+
+  const handleSignInWithApple = async () => {
+    setIsLoadingApple(true);
+    if (isLoaded && signIn) {
+      await signIn.authenticateWithRedirect({
+        redirectUrl: "/sso",
+        redirectUrlComplete: "/",
+        strategy: "oauth_apple",
+      });
+    }
+    setIsLoadingApple(false);
+  };
+
   return (
-    <div className="h-screen pl-4 pt-4">
-      <section className="flex h-full flex-col items-center justify-center rounded-tl-2xl border border-slate-300/50 bg-white p-4 shadow-inner">
-        <SignIn
+    <AuthLayout linkHref="sign-up">
+      <div className="flex w-full max-w-96 flex-col">
+        <AuthHeader title="Sign In" />
+
+        <>
+          <div className="pb-2 pt-4">
+            <Button
+              className="w-full justify-center"
+              disabled={isLoadingApple}
+              loading={isLoadingApple}
+              theme={ButtonTheme.Outline}
+              onClick={handleSignInWithApple}
+            >
+              <span>Continue with Apple</span>
+            </Button>
+          </div>
+
+          <div className="flex items-center justify-center gap-3 pt-2">
+            <hr className="w-full" />
+            <span className="text-zinc-600 dark:text-zinc-200">or</span>
+            <hr className="w-full" />
+          </div>
+        </>
+
+        {/* <SignIn
           path="/sign-in"
           routing="path"
           signUpUrl="/sign-up"
@@ -13,8 +60,8 @@ export default function SignInPage() {
               card: "shadow-none p-0 flex flex-col",
             },
           }}
-        />
-      </section>
-    </div>
+        /> */}
+      </div>
+    </AuthLayout>
   );
 }

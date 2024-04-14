c98d42e8a98e397dcdcf4c3f2825f26134b31f44
Devin Sharpe <devin.sharpe@telrite.com>
2024-04-14 12:13:28 -0400

    feat: add sign in page template and logic

--- apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,124 @@
 "use client";
 
-import React from "react";
+import React, { FormEvent } from "react";
 import { useSignIn } from "@clerk/nextjs";
 import AuthHeader from "~/components/AuthHeader";
 import AuthLayout from "~/layouts/Auth";
-import { Button, ButtonTheme } from "@repo/ui/components";
+import {
+  Button,
+  ButtonSize,
+  ButtonTheme,
+  FloatingInput,
+} from "@repo/ui/components";
 import useAuthGuard from "~/hooks/useAuthGuard";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+interface AccountCredentials {
+  emailAddress: string;
+  password: string;
+}
+
+interface AccountFormProps {
+  errors: Partial<AccountCredentials>;
+  isLoading: boolean;
+  value: AccountCredentials;
+  onChange: (val: AccountCredentials) => void | Promise<void>;
+  onSubmit: (val: AccountCredentials) => void | Promise<void>;
+}
+
+function AccountForm({
+  errors,
+  isLoading,
+  value,
+  onChange,
+  onSubmit,
+}: AccountFormProps) {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit(value);
+  };
+
+  return (
+    <form className="flex w-full flex-col gap-4 pt-4" onSubmit={handleSubmit}>
+      <FloatingInput.Fieldset>
+        <FloatingInput.Input
+          id="email"
+          type="email"
+          name="email"
+          autoComplete="email"
+          placeholder="jane.doe@email.com"
+          required
+          errorMessage={errors.emailAddress}
+          value={value.emailAddress}
+          onChange={(e) => onChange({ ...value, emailAddress: e.target.value })}
+        />
+        <FloatingInput.Label htmlFor="email">Email Address</FloatingInput.Label>
+      </FloatingInput.Fieldset>
+
+      <FloatingInput.Fieldset>
+        <FloatingInput.Input
+          id="password"
+          type="password"
+          name="password"
+          autoComplete="new-password"
+          placeholder="password"
+          required
+          errorMessage={errors.password}
+          value={value.password}
+          onChange={(e) => onChange({ ...value, password: e.target.value })}
+        />
+        <FloatingInput.Label htmlFor="password">Password</FloatingInput.Label>
+      </FloatingInput.Fieldset>
+
+      <Button
+        className="justify-center"
+        disabled={isLoading}
+        loading={isLoading}
+        size={ButtonSize.Lg}
+        theme={ButtonTheme.Primary}
+        type="submit"
+      >
+        <span>Continue to App</span>
+      </Button>
+
+      <p>
+        Need to create an account?&nbsp;
+        <Link
+          href="/sign-up"
+          className="rounded-sm text-zinc-600 underline focus:outline-none focus:ring focus:ring-zinc-600/50 dark:text-white"
+        >
+          Sign Up
+        </Link>
+      </p>
+    </form>
+  );
+}
+
+interface ClerkError {
+  code: string;
+  message: string;
+  longMessage: string;
+  meta: {
+    paramName?: string;
+  };
+}
 
 export default function SignInPage() {
   const { isLoaded, signIn, setActive } = useSignIn();
+  const [form, setForm] = React.useState<AccountCredentials>({
+    emailAddress: "",
+    password: "",
+  });
+
+  const [accountFormErrors, setAccountFormErrors] = React.useState<
+    Partial<AccountCredentials>
+  >({});
 
   const [isLoadingApple, setIsLoadingApple] = React.useState(false);
+  const [isLoadingEmail, setIsLoadingEmail] = React.useState(false);
+
+  const router = useRouter();
 
   useAuthGuard(true, "/");
 
@@ -24,6 +132,43 @@ export default function SignInPage() {
       });
     }
     setIsLoadingApple(false);
+  };
+
+  const handleEmailRequest = async (formVal: AccountCredentials) => {
+    try {
+      if (!signIn) return;
+
+      setIsLoadingEmail(true);
+
+      const result = await signIn.create({
+        identifier: formVal.emailAddress,
+        password: formVal.password,
+      });
+
+      console.log(result);
+
+      // TODO: handle other statuses
+      if (result.status === "complete") {
+        await setActive({ session: result.createdSessionId });
+        router.push("/app");
+      } else console.log(result.status, result);
+    } catch (err: any) {
+      if (err.ClerkError) {
+        setAccountFormErrors(
+          err.errors.reduce(
+            (prev: typeof accountFormErrors, curr: ClerkError) => {
+              if (curr.meta.paramName === "email_address")
+                prev.emailAddress = curr.message;
+              if (curr.meta.paramName === "password")
+                prev.password = curr.message;
+              return prev;
+            },
+            {},
+          ),
+        );
+      } else console.log(JSON.stringify(err, null, 2));
+    }
+    setIsLoadingEmail;
   };
 
   return (
@@ -49,18 +194,15 @@ export default function SignInPage() {
             <span className="text-zinc-600 dark:text-zinc-200">or</span>
             <hr className="w-full" />
           </div>
-        </>
 
-        {/* <SignIn
-          path="/sign-in"
-          routing="path"
-          signUpUrl="/sign-up"
-          appearance={{
-            elements: {
-              card: "shadow-none p-0 flex flex-col",
-            },
-          }}
-        /> */}
+          <AccountForm
+            errors={accountFormErrors}
+            isLoading={isLoadingEmail}
+            onChange={setForm}
+            onSubmit={handleEmailRequest}
+            value={form}
+          />
+        </>
       </div>
     </AuthLayout>
   );
